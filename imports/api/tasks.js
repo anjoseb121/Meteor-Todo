a5fcdc35f43d306854a0d96efe032645aecf7ef6
AntonioBar <anjose.121@gmail.com>
2017-03-10 11:00:12 -0500

    Added input to add tasks

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,3 +1,3 @@
 import { Mongo } from 'meteor/mongo';
-
+// Create tasks collection in mongo
 export const Tasks = new Mongo.Collection('tasks');
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks.js';
@@ -7,17 +8,37 @@ import Task from './Task.jsx';
 
 // App component - represents the whole app
 class App extends Component {
+
+	handleSubmit(event) {
+		event.preventDefault();
+
+		// Find the text field via react ref
+		const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+		Tasks.insert({
+			text,
+			createdAt: new Date(), // current time
+		});
+
+		// Clear form
+		ReactDOM.findDOMNode(this.refs.textInput).value = '';
+	}
+
 	renderTasks() {
+		// Return objects from props
 		return this.props.tasks.map((task) => (
 			<Task key={task._id} task={task} />
 		));
 	}
-
+	// Render method to client
 	render() {
 		return (
 			<div className="container">
 				<header>
 					<h1>Todo List</h1>
+					<form onSubmit={this.handleSubmit.bind(this)} className="new-task">
+						<input type="text" ref="textInput" placeholder="Type to add new tasks"/>
+					</form>
 				</header>
 
 				<ul>
@@ -29,11 +50,12 @@ class App extends Component {
 }
 
 App.propTypes = {
+	// Props said is an array
 	tasks: PropTypes.array.isRequired,
 };
 
 export default createContainer(() => {
 	return {
-		tasks: Tasks.find({}).fetch(),
+		tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
 	};
 }, App);
