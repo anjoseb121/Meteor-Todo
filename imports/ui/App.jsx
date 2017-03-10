6be00012692031f7b43616614ddf575c4d2237d1
AntonioBar <anjose.121@gmail.com>
2017-03-09 23:59:03 -0500

    Added main css

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -6,7 +6,7 @@ export default class App extends Component {
 
 	getTasks() {
 		return [
-			{ _id: 1, text: 'This is a task 1' },
+			{ _id: 1, text: 'This is a task 1 ' },
 			{ _id: 2, text: 'This is a task 2' },
 			{ _id: 3, text: 'This is a task 3' },
 		];
