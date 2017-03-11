4251e2115810e4035309890bda65d6b97c3e4ae7
AntonioBar <anjose.121@gmail.com>
2017-03-10 20:51:47 -0500

    Bug can set checked tasks from anothers users

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -35,14 +35,25 @@ Meteor.methods({
 	},
 	'tasks.remove'(taskId) {
 		check(taskId, String);
+		const task = Tasks.findOne(taskId);
+		if (task.private && task.owner !== this.userId) {
+			// If the task is private, make sure onlye the owner can delete it
+			throw new Meteor.Error('not-authorized');
+		}
 		Tasks.remove(taskId);
 	},
 	'tasks.setChecked'(taskId, setChecked) {
-		check(taskId, String);
-		check(setChecked, Boolean);
+	    check(taskId, String);
+	    check(setChecked, Boolean);
 
-		Tasks.update(taskId, { $set: { checked: setChecked } });
-	},
+	    const task = Tasks.findOne(taskId);
+	    if (task.private && task.owner !== this.userId) {
+	      // If the task is private, make sure only the owner can check it off
+	      throw new Meteor.Error('not-authorized');
+	    }
+
+	    Tasks.update(taskId, { $set: { checked: setChecked } });
+	  },
 	'tasks.setPrivate'(taskId, setToPrivate) {
 		check(taskId, String);
 		check(setToPrivate, Boolean);
